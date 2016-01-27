2db28f0ea70dbb844d5645328cd834428d7f513a
tiecheng <tiecheng@17shihui.com>
2016-01-27 11:39:01 +0800

    添加数据表bean

---
+++ open-home-app/open-home-service-impl/src/main/java/com/shihui/openpf/home/dao/MerchantGoodsDao.java
@@ -0,0 +1,11 @@
+package com.shihui.openpf.home.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by zhoutc on 2016/1/27.
+ */
+@Repository
+public class MerchantGoodsDao extends AbstractDao<MerchantGoodsDao>{
+
+}
