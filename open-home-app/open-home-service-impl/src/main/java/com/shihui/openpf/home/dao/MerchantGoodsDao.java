d97f9ddb246f0c3d3c26a3a27cf2a6599563aa16
tiecheng <tiecheng@17shihui.com>
2016-01-27 17:10:15 +0800

    修改dao

--- open-home-app/open-home-service-impl/src/main/java/com/shihui/openpf/home/dao/MerchantGoodsDao.java
+++ open-home-app/open-home-service-impl/src/main/java/com/shihui/openpf/home/dao/MerchantGoodsDao.java
@@ -1,11 +1,12 @@
 package com.shihui.openpf.home.dao;
 
+import com.shihui.openpf.home.model.MerchantGoods;
 import org.springframework.stereotype.Repository;
 
 /**
  * Created by zhoutc on 2016/1/27.
  */
 @Repository
-public class MerchantGoodsDao extends AbstractDao<MerchantGoodsDao>{
+public class MerchantGoodsDao extends AbstractDao<MerchantGoods>{
 
 }
