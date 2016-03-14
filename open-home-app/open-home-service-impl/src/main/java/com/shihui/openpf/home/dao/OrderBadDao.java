d4692b44f331fc522b4c5a2bf65f6c4503ae0022
qisheng <qisheng@17shihui.com>
2016-03-14 14:45:29 +0800

    增加异常订单记录

---
+++ open-home-app/open-home-service-impl/src/main/java/com/shihui/openpf/home/dao/OrderBadDao.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.shihui.openpf.home.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.shihui.openpf.home.model.OrderBad;
+
+/**
+ * @author zhouqisheng
+ * @date 2016年3月14日 下午2:16:35
+ *
+ */
+@Repository
+public class OrderBadDao extends AbstractDao<OrderBad> {
+
+}
