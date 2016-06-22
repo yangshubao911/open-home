39a735ab1acc7e8fe5dae3f0e805a34b9dd34f3a
zhouqisheng <qisheng@17shihui.com>
2016-06-22 16:01:13 +0800

    修改启动bug

--- open-home-app/open-home-service-impl/src/main/java/com/shihui/openpf/home/dao/CategoryRankDao.java
+++ open-home-app/open-home-service-impl/src/main/java/com/shihui/openpf/home/dao/CategoryRankDao.java
@@ -3,12 +3,15 @@
  */
 package com.shihui.openpf.home.dao;
 
+import org.springframework.stereotype.Repository;
+
 import com.shihui.openpf.home.model.CategoryRank;
 
 /**
  * @author zhouqisheng
  *
  */
+@Repository
 public class CategoryRankDao extends AbstractDao<CategoryRank> {
 
 }
